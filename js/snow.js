321aca1c055a0ec53e9f6f013bbb38760df35709
Jayadev <jaidev.akkiraju@gmail.com>
2015-03-14 16:11:43 -0700

    added calvin background

--- js/snow.js
+++ js/snow.js
@@ -1,26 +1,17 @@
 (function(){
 	
 	function init(){
-			var vid = document.getElementById("audio");
-			vid.autoplay = true;
-			// vid.load();
-			
+			var audio = document.getElementById("audio");
+			audio.autoplay = true;
+
 			var canvas=document.getElementsByTagName("canvas")[0];
 			canvas.width=window.innerWidth;
 			canvas.height=window.innerHeight;
 			var ctx = canvas.getContext("2d");
 
-			var pic=new Image();
-			pic.src="images/snow.jpg";	
-			ctx.drawImage(pic,0,0,canvas.width,canvas.height);
-
-			// pic.addEventListener("load",function(){
-			// 	ctx.drawImage(pic,0,0,canvas.width,canvas.height);
-			// });
+		
 			window.onresize=function(event){
-				canvas.width=window.innerWidth;
-				canvas.height=window.innerHeight;
-				ctx.drawImage(pic,0,0,canvas.width,canvas.height);	
+				ctx.clearRect(0,0,canvas.width,canvas.height);
 			}
 
 			var flakes=[];
@@ -37,13 +28,13 @@
 				addFlake();
 				
 				for(var i=0;i<flakes.length;i++){
-					ctx.fillStyle="rgba(255,255,255,0.75)";
+					ctx.fillStyle="rgba(255,255,210,0.75)";
 					ctx.beginPath();
 					ctx.arc(flakes[i].x,flakes[i].y,flakes[i].r,0,2*Math.PI,false);
 					ctx.fill();
-					
+
 					if(flakes[i].y<300){
-						flakes[i].x+=4.5;
+						flakes[i].x+=4.0;
 						flakes[i].y+=1.5;
 					}
 
@@ -65,6 +56,7 @@
 					else{
 						// remove the flake once it reaches the bottom of the canvas
 						flakes.splice(i,1);
+						flakes.splice(i,1);
 
 					}
 				}
@@ -73,14 +65,12 @@
 			function animate(){
 				// redraw the canvas frame by frame
 				ctx.clearRect(0,0,canvas.width,canvas.height);
-				ctx.drawImage(pic,0,0,canvas.width,canvas.height);
+				// ctx.drawImage(pic,0,0,canvas.width,canvas.height);
 				snow();
 			}
-
-
+			
 			// Call animate function continuously for every 20 milliseconds
 			var animateInterval=setInterval(animate,20);
-			// var boxInterval=setInterval(box,35);
 	}
 	window.addEventListener('load',init,false);
 }());
